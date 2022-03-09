ed1989aea54de9757b9b91c77b35ebe468c79fdd
Austin Eral <austineral@ymail.com>
2022-03-09 01:23:47 -0800

    Finished code

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,96 @@
 use std::sync::{Arc, Mutex};
-use threadpool::ThreadPool;
+use std::time::Instant;
+use threadpool::{Builder, ThreadPool};
+
+struct Ptr(*mut [i32]);
+
+unsafe impl Send for Ptr {}
+
+const NUM_ELEMENTS: usize = 100_000_000;
+const NUM_THREADS: usize = 16;
+const NUM_ELS_PER_SLICE: usize = NUM_ELEMENTS / NUM_THREADS;
+const THREAD_STACK_SIZE: usize = 1_000_000;
 
 fn main() {
-    const NUM_ELEMENTS: usize = 9;
-    const NUM_THREADS: usize = 8;
-    const NUM_ELS_PER_SLICE: usize = NUM_ELEMENTS / NUM_THREADS;
-
-    let mut vec = Vec::<i32>::from([0; NUM_ELEMENTS]);
-    let pool = ThreadPool::new(NUM_THREADS);
-
-
-    let mut slices = Vec::<*mut [i32]>::new();
-    for i in 0..NUM_THREADS {
-        let start = i * NUM_ELS_PER_SLICE;
-        let end = start + NUM_ELS_PER_SLICE;
-        let slice = &mut vec[start..end] as *mut [i32];
-        slices.push(slice);
-    }
-
-    unsafe {
-        let mut count = 0;
-        for slice in slices {
-            // print slice
-            if count == NUM_THREADS - 1 {
-                println!("{}", (*slice)[1]);
-            }
-            count += 1;
-        }
-    }
-    // unsafe {
-    //     (*raw)[1] = 1;
-    //     println!("{:?}", vec);
-    // }
+    // time
+    let start = Instant::now();
+    let vec = run_safe();
+    println!("Safe run time: {:?}", start.elapsed());
+    assert_eq!(vec, vec![1; NUM_ELEMENTS]);
+    let start = Instant::now();
+    let vec = run_unsafe();
+    println!("Unsafe run time: {:?}", start.elapsed());
+    assert_eq!(vec, vec![1; NUM_ELEMENTS]);
 }
 
-// fn main() {
-//     let mutex = Arc::new(Mutex::new(vec![0; 10]));
-//     let mut threads = vec![];
+fn run_safe() -> Vec<i32> {
+    let vec = vec![0; NUM_ELEMENTS];
+    let mutex = Arc::new(Mutex::new(vec));
+    let pool = ThreadPool::new(NUM_THREADS);
 
-//     let mutex1 = mutex.clone();
-//     threads.push(std::thread::spawn(move || {
-//         let mut vec = mutex1.lock().unwrap();
-//         vec[0] = 1;
-//     }));
-//     let mutex2 = mutex.clone();
-//     threads.push(std::thread::spawn(move || {
-//         let mut vec = mutex2.lock().unwrap();
-//         vec[1] = 1;
-//     }));
-    
-//     for thread in threads {
-//         thread.join().unwrap();
-//     }
-//     println!("{:?}", mutex.lock().unwrap());
-// }
+    let mut ranges = Vec::<(usize, usize)>::new();
+    let mut last_end = 0;
+    for i in 0..NUM_THREADS {
+        let start = last_end;
+        let mut end = start + NUM_ELS_PER_SLICE;
+        if (NUM_ELEMENTS - last_end) % (NUM_THREADS - i) != 0 {
+            end += 1;
+        }
+        let range = (start, end);
+        ranges.push(range);
+        last_end = end;
+    }
+
+    for range in ranges {
+        let mutex_clone = mutex.clone();
+        pool.execute(move || {
+            let mut vec = mutex_clone.lock().unwrap();
+            for i in range.0..range.1 {
+                vec[i] += 1 as i32;
+            }
+        });
+    }
+
+    pool.join();
+
+    let vec = mutex.lock().unwrap().to_vec();
+    vec
+}
+
+fn run_unsafe() -> Vec<i32> {
+    let mut vec = vec![0; NUM_ELEMENTS];
+    let pool = Builder::new()
+        .num_threads(NUM_THREADS)
+        .thread_stack_size(THREAD_STACK_SIZE)
+        .build();
+
+    let mut slices = Vec::<(Ptr, usize)>::new();
+    let mut last_end = 0;
+    for i in 0..NUM_THREADS {
+        let start = last_end;
+        let mut end = start + NUM_ELS_PER_SLICE;
+        if (NUM_ELEMENTS - last_end) % (NUM_THREADS - i) != 0 {
+            end += 1;
+        }
+        let slice = Ptr(&mut vec[start..end]);
+        let size = end - start;
+        slices.push((slice, size));
+        last_end = end;
+    }
+
+    for mutex in slices {
+        pool.execute(move || {
+            let slice = mutex.0;
+            let size = mutex.1;
+            for i in 0..size {
+                unsafe {
+                    (*slice.0)[i] += 1 as i32;
+                }
+            }
+        });
+    }
+
+    pool.join();
+
+    vec
+}
